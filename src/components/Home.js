b841d15955a5b6df12b4544531a72dd3761094e2
Junaid <qjunaid623@gmail.com>
2020-10-17 15:47:47 +0500

    Added some animations to Nav and Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,60 @@
 import React from "react";
-import './../index.css';
-import man from './../images/man.svg';
+import "./../index.css";
+import man from "./../images/man.svg";
+import { motion } from "framer-motion";
+
+const contentVariants = {
+  initial: {
+    translateY: "100vh",
+    opacity: 0,
+  },
+
+  animate: {
+    translateY: "0vh",
+    opacity: 1,
+    transition: {
+      duration: 2,
+      type: "spring",
+    },
+  },
+};
 
 const Home = () => {
-    return (
-        <div className="home-container">
-            <div className='content-container'>
-                <h4 className='welcome-content'>WELCOME TO MY WORLD</h4><br />
-                <h1 className='main-content'>Hi, I’m Junaid  👋 </h1>
-                <h2 className='skill-content'>( Web Developer | Open Source Contributor )</h2>
-                <h1> from Pakistan 🇵🇰</h1>
-            </div>
+  return (
+    <div className="home-container">
+      <motion.div
+        className="content-container"
+        variants={contentVariants}
+        initial="initial"
+        animate="animate"
+      >
+        <h4 className="welcome-content">WELCOME TO MY WORLD</h4>
+        <br />
+        <h1 className="main-content">
+          Hi, I’m Junaid
+          <motion.span
+            className="hand"
+            animate={{ rotate: [0, 20, 0, 20, 0, 0, 0, 0, 0, 0] }}
+            transition={{ yoyo: Infinity, duration: 1.7 }}
+          >
+            👋
+          </motion.span>{" "}
+        </h1>
+        <h2 className="skill-content">
+          ( Web Developer | Open Source Contributor )
+        </h2>
+        <h1> from Pakistan 🇵🇰</h1>
+      </motion.div>
 
-            <div className="svg-container">
-                <img className='svg' src={man} alt="" />
-            </div>
-
-        </div>
-    );
+      <motion.div
+        className="svg-container"
+        animate={{ translateY: [-20, 0, -20, 0] }}
+        transition={{ yoyo: Infinity, duration: 6 }}
+      >
+        <img className="svg" src={man} alt="" />
+      </motion.div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,44 @@
 import React from "react";
-import './../index.css';
+import "./../index.css";
+import { motion } from "framer-motion";
 
 const Nav = () => {
-    return (
-        <div className="nav-container">
-            <span className='logo'>&lt; Dev-Junaid /&gt;</span>
-            <ul className='nav-links'>
-                <li className='link'><a href='#'>Home</a></li>
-                <li className='link'><a href='#'>Services</a></li>
-                <li className='link'><a href='#'>About</a></li>
-                <li className='link'><a href='#'>Achievements</a></li>
-                <li className='link'><a href='#'>Portfolio</a></li>
-                <li className='link'><a href='#'>Contact</a></li>
-            </ul>
-        </div>
-    );
+  return (
+    <motion.div
+      className="nav-container"
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1.5,
+      }}
+    >
+      <span className="logo">&lt; Dev-Junaid /&gt;</span>
+      <ul className="nav-links">
+        <li className="link">
+          <a href="#">Home</a>
+        </li>
+        <li className="link">
+          <a href="#">Services</a>
+        </li>
+        <li className="link">
+          <a href="#">About</a>
+        </li>
+        <li className="link">
+          <a href="#">Achievements</a>
+        </li>
+        <li className="link">
+          <a href="#">Portfolio</a>
+        </li>
+        <li className="link">
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </motion.div>
+  );
 };
 
 export default Nav;

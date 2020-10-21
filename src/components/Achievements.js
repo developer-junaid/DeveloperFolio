e8288c0b29870cc639cf8e91b3afe564773cd42e
Junaid <qjunaid623@gmail.com>
2020-10-21 15:24:51 +0500

    Completed Home animation

--- src/components/Achievements.js
+++ src/components/Achievements.js
@@ -9,6 +9,153 @@ const Achievements = () => {
       id="achievements"
     >
       <h1 className="heading">Achievements</h1>
+      <div className="projects-container">
+        {/* Project 1 - React Basic */}
+        <div className="projects">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.hackerrank.com/certificates/6b34ec5a135a"
+          >
+            <div className="project-images" id="reactBasic"></div>
+          </a>
+
+          <div className="project-links">
+            <div className="text">
+              <h3>React Basic (HackerRank)</h3>
+            </div>
+            <div className="icons">
+              <a
+                rel="noopener noreferrer"
+                href="https://www.hackerrank.com/certificates/6b34ec5a135a"
+                target="_blank"
+              >
+                <i
+                  className="fas fa-external-link-alt"
+                  title="live preview"
+                  id="live"
+                ></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/* Project 2 - HTML5 */}
+        <div className="projects">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.coursera.org/account/accomplishments/certificate/G83XS5DQJCN8"
+          >
+            <div className="project-images" id="html5"></div>
+          </a>
+
+          <div className="project-links">
+            <div className="text">
+              <h3>Intro to HTML5 (Michigan University)</h3>
+            </div>
+            <div className="icons">
+              <a
+                rel="noopener noreferrer"
+                href="https://www.coursera.org/account/accomplishments/certificate/G83XS5DQJCN8"
+                target="_blank"
+              >
+                {" "}
+                <i
+                  className="fas fa-external-link-alt"
+                  title="url"
+                  id="live"
+                ></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/* Project 3 - Cisco Python */}
+        <div className="projects">
+          <a rel="noopener noreferrer" target="_blank" href="#">
+            <div className="project-images" id="ciscoPython"></div>
+          </a>
+
+          <div className="project-links">
+            <div className="text">
+              <h3>Programming Associate in Python (Cisco)</h3>
+            </div>
+            <div className="icons">
+              <a rel="noopener noreferrer" href="#" target="_blank">
+                {" "}
+                <i
+                  className="fas fa-external-link-alt"
+                  title="url"
+                  id="live"
+                ></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/* Project 4 - Git */}
+        <div className="projects">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.coursera.org/account/accomplishments/certificate/37AM696AMTE5"
+          >
+            <div className="project-images" id="git"></div>
+          </a>
+
+          <div className="project-links">
+            <div className="text">
+              <h3>Version Control using Git (Atlassian)</h3>
+            </div>
+            <div className="icons">
+              <a
+                rel="noopener noreferrer"
+                href="https://www.coursera.org/account/accomplishments/certificate/37AM696AMTE5"
+                target="_blank"
+              >
+                {" "}
+                <i
+                  className="fas fa-external-link-alt"
+                  title="url"
+                  id="live"
+                ></i>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/* Project 5 - Specialization Python */}
+        <div className="projects">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.coursera.org/account/accomplishments/specialization/certificate/DGENHCAN2NWE"
+          >
+            <div className="project-images" id="specPython"></div>
+          </a>
+
+          <div className="project-links">
+            <div className="text">
+              <h3>Python Specialization (Michigan University)</h3>
+            </div>
+            <div className="icons">
+              <a
+                rel="noopener noreferrer"
+                href="https://www.coursera.org/account/accomplishments/specialization/certificate/DGENHCAN2NWE"
+                target="_blank"
+              >
+                {" "}
+                <i
+                  className="fas fa-external-link-alt"
+                  title="url"
+                  id="live"
+                ></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./../index.css";
 import man from "./../images/man.svg";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { animateScroll as scroll } from "react-scroll";
 
 const contentVariants = {
@@ -16,11 +16,55 @@ const contentVariants = {
     transition: {
       duration: 2,
       type: "spring",
+      when: "beforeChildren",
+      // staggerChildren: 0.4,
     },
   },
 };
 
+const childrenVariants = {
+  initial: {
+    opacity: 0,
+    y: 100
+  },
+
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.7,
+      type: 'spring',
+      delay: 0.5
+    }
+  },
+
+  exit: {
+    opacity: 0,
+    y: -200,
+    transition: { duration: 0.2 }
+  }
+
+}
+
 const Home = () => {
+  // State
+  const [showHeadingOne, setShowHeadingOne] = useState(true);
+  const [showHeadingTwo, setShowHeadingTwo] = useState(false);
+
+  // Timeout
+  useEffect(() => {
+    setTimeout(() => {
+      if (showHeadingOne) {
+        setShowHeadingOne(false);
+        setShowHeadingTwo(true);
+      } else {
+        setShowHeadingOne(true);
+        setShowHeadingTwo(false);
+      }
+    }, 3000)
+  }, [showHeadingOne, showHeadingTwo])
+
+
   return (
     <section className="home-container" id="home" name="home">
       <motion.div
@@ -34,39 +78,71 @@ const Home = () => {
         <h1 className="main-content">
           Hi, I’m Junaid
           <motion.span
+            drag={true}
+            dragConstraints={{left:0, top:0, bottom:0, right:0} }
             className="hand"
             animate={{ rotate: [0, 20, 0, 20, 0, 0, 0, 0, 0, 0] }}
             transition={{ yoyo: Infinity, duration: 1.7 }}
           >
             <span role="img" aria-label="Hand waving">
-              👋
+            👋
             </span>
           </motion.span>{" "}
         </h1>
-        <h2 className="skill-content">
-          Web UI/UX Developer | React Developer | Open Source Contributor
-        </h2>
-        <h1>
-          {" "}
-          from Pakistan{" "}
-          <span role="img" aria-label="Pakistan Flag">
-            🇵🇰
-          </span>
-        </h1>
+      {/* Animate Skill Content */}
 
-        <button className="button" onClick={() => scroll.scrollToBottom()}>
-          Contact Me
+      <div className="skill-animation">
+        <AnimatePresence>
+          {showHeadingOne && (
+            <motion.h2 className="skill-content"
+              variants={childrenVariants}
+              exit='exit'
+              animate='animate'
+              initial='initial'
+            >
+
+              Web UI/UX Developer | Open Source Contributor
+            </motion.h2>
+          )}
+        </AnimatePresence>
+
+        <AnimatePresence>
+          {showHeadingTwo && (
+            <motion.h2
+              className="skill-content"
+              variants={childrenVariants}
+              exit='exit'
+              animate='animate'
+              initial='initial'
+            >
+              React Developer
+            </motion.h2>
+          )}
+        </AnimatePresence>
+      </div>
+      {/* // */}
+
+      <h1>
+        {" "}
+          from Pakistan{" "}
+        <span role="img" aria-label="Pakistan Flag">
+          🇵🇰
+          </span>
+      </h1>
+
+      <button className="button" onClick={() => scroll.scrollToBottom()}>
+        Contact Me
         </button>
       </motion.div>
 
-      <motion.div
-        className="svg-container"
-        animate={{ translateY: [-20, 0, -20, 0] }}
-        transition={{ yoyo: Infinity, duration: 6 }}
-      >
-        <img className="svg" src={man} alt="" />
-      </motion.div>
-    </section>
+    <motion.div
+      className="svg-container"
+      animate={{ translateY: [-20, 0, -20, 0] }}
+      transition={{ yoyo: Infinity, duration: 6 }}
+    >
+      <img className="svg" src={man} alt="" />
+    </motion.div>
+    </section >
   );
 };
 

5ccb6c117c015ed057c1bbf08462d723a1f3c9e0
developer-junaid <qjunaid623@gmail.com>
2021-10-10 09:18:22 +0500

    completed portfolio section

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,28 +1,80 @@
-import React from "react";
-import { PortfolioCard } from "./PortfolioCard/PortfolioCard";
+import React, { useEffect, useState } from "react";
+
+// Styles
 import "./portfolio.css";
+
+// Data
+import {
+  portfolioList,
+  featuredPortfolio,
+  mobilePortfolio,
+  webPortfolio,
+} from "data/data";
+
+// components
 import { Heading } from "components/Heading/Heading";
+import PortfolioList from "./PortfolioList/PortfolioList";
 
 const Portfolio = () => {
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    switch (selected) {
+      case "featured":
+        setData(featuredPortfolio);
+        break;
+
+      case "web":
+        setData(webPortfolio);
+        break;
+
+      case "mobile":
+        setData(mobilePortfolio);
+        break;
+
+      default:
+        break;
+    }
+  }, [selected]);
+
   return (
     <section
       data-aos="fade-left"
-      className="portfolio-container"
+      className="portfolio"
       name="portfolio"
       id="portfolio"
     >
       <Heading text="Portfolio" />
-      <div className="projects-container">
-        <PortfolioCard />
+      <ul>
+        {portfolioList &&
+          portfolioList.map((list) => (
+            <PortfolioList
+              title={list.title}
+              key={list.id}
+              active={selected === list.id}
+              setSelected={setSelected}
+              id={list.id}
+            />
+          ))}
+      </ul>
+      <div className="container">
+        {data &&
+          data.map((item) => (
+            <div className="item">
+              <img src={item.img} alt={item.title} />
+              <h3>{item.title}</h3>
+            </div>
+          ))}
       </div>
-      <a
+      {/* <a
         rel="noopener noreferrer"
         target="_blank"
         href="https://github.com/developer-junaid/"
         className="button-link"
       >
         <button className="button">See More</button>
-      </a>
+      </a> */}
     </section>
   );
 };
